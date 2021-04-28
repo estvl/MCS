e55bcff8a24fe5065398afc6e02cc5d224aaa2b6
estvl <esat.shehaj@gmail.com>
2021-04-28 15:47:14 +0200

    f

--- BookOrderAngularFrontend/book-order/src/app/sky/sky.component.ts
+++ BookOrderAngularFrontend/book-order/src/app/sky/sky.component.ts
@@ -1,3 +1,4 @@
+import { SkyService } from './../sky.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private skyService: SkyService) { }
 
   ngOnInit() {
   }
